5dac1fbe6517b535d3babdcc68f6fa714fca27a0
ayoubensalem <ayoubensalem@gmail.com>
2022-07-14 16:47:30 +0100

    bump version to 3.0.4

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("readme.rst", "r") as f:
 
 setup(
     name="arvos",
-    version="3.0.3",
+    version="3.0.4",
     description="Arvos Command Line Utility",
     long_description=readme,
     author='Ayoub ED-DAFALI',
